d15d856cebc919bbd8a7bcbb0fcd3f381f0523c7
Sujeet17m <sujeet17das@gmail.com>
2025-06-16 09:27:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-import { log } from 'node:console';
-import http from 'node:http';
+import http from 'node:http'//node: bcoz browser can confuse http between js & node
 
-const PORT = 8000
+const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-    res.end('Hello from the server!')
+const server = http.createServer((req,res) => {
+    console.log(req.url);
 
-});
+    if (req.url === '/api'){
+        res.end('This is from the server')
+    } else {
+        res.end('Not Found')
+    }
+})
 
-server.listen(PORT , () => console.log(`server running on ${PORT}`)
-)
+server.listen(PORT , () => console.log(`connected on port : ${PORT}`))
